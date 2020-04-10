27d6c5188d1a92f63df06146391004cf9a9006ec
Jasmine Lok <jasminellok@gmail.com>
2020-04-10 20:13:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ function johnLennonFacts(facts) {
   }
   return newFacts
 }
+
+function iLoveTheBeatles(num) {
+  let i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i
+  }
+  do {
+  console.log("I love the Beatles!");
+} while (incrementVariable() < num);
+}
