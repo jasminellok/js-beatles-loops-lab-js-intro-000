125fd04c2d6470e6a17a4ecc1843b341947eac40
Jasmine Lok <jasminellok@gmail.com>
2020-04-10 19:24:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments) {
   emptyArray= []
   for (let i=0; i<musicians.length; i+=1) {
-    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+    emptyArray.push(`'${musicians[i]} plays ${instruments[i]}'`)
   }
   return emptyArray
 }
